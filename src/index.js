cbfa5abda979c52028070d2096ed2299457cb7cb
Elżbieta Kukułka <ellakaras@gmail.com>
2018-04-19 17:41:11 +0200

    Download babel-preset-es2015, babel-preset-react, react-hot-loader, html-webpack-plugin

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
-import uuid from 'uuid';
-import style from './App.css';
+
 
 ReactDOM.render(
     <App />,
