2bf3d599475f17e57384bab9541a1c8219fecfa0
Elżbieta Kukułka <ellakaras@gmail.com>
2018-05-10 17:41:20 +0200

    Implement component Title

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import uuid from 'uuid';
 import style from './App.css';
+import Title from '../components/Title';
 
 class App extends React.Component {
     constructor(props){
@@ -26,6 +27,7 @@ class App extends React.Component {
     render() {
         return (
             <div className={style.TodoApp}>
+                <Title title="ToDo App" number={this.state.data.length}></Title>
                 Tutaj pojawią się komponenty naszej aplikacji.
             </div>
         );
